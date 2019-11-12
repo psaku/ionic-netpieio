8c376a2160fc96b57f48574256b4a57ba6553b42
Prasertsak U <41256576+psaku@users.noreply.github.com>
2019-11-12 09:20:34 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ResultPage } from '../pages/result/result';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ResultPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,{
+      mode: "ios"
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ResultPage
   ],
   providers: [
     StatusBar,
